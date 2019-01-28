25389f313ef941dd63557e69bcb567b8c4a53d8a
Jaehyun Song <jaehyunsong@Jaehyuns-MacBook-Pro.local>
2019-01-28 20:34:04 +0900

    First commit

---
+++ R/GenerateURL.R
@@ -0,0 +1,36 @@
+GenerateURL <- function(file, Task = 3, Profile = 2,
+                        Randomized = TRUE){
+
+  df <- read.csv(file, header = FALSE, stringsAsFactors = FALSE)
+  nA <- ncol(df)
+  nL <- c()
+  A  <- c()
+  L  <- c()
+
+  j  <- 1
+
+  for (i in 1:nA) {
+    Temp.vec <- df[, i]
+    A[i]  <- Temp.vec[1]
+    nL[i] <- sum(Temp.vec != "") - 1
+    L[j:(j + nL[i] - 1)] <- Temp.vec[2:(nL[i] + 1)]
+    j <- j + nL[i]
+  }
+
+  nA.R <- paste0("nA=", nA)
+  nL.R <- paste0("nL[]=", paste(nL, collapse = "&nL[]="))
+  A.R  <- paste0("A[]=", paste(A, collapse = "&A[]="))
+  L.R  <- paste0("L[]=", paste(L, collapse = "&L[]="))
+  nPro <- paste0("nProfile=", Profile)
+  nTas <- paste0("nTask=", Task)
+  Rand <- paste0("AttrRand=", as.numeric(Randomized))
+
+  result <- paste0("http://www.jaysong.net/Qualtrics/SimpleConjoint/SimpleConjoint.php?",
+                   paste(c(nTas, nPro, Rand, nA.R, nL.R, A.R, L.R),
+                         collapse = "&"))
+
+  result <- gsub("&", "%26", result, fixed = TRUE)
+  result <- gsub("?", "%3F", result, fixed = TRUE)
+
+  return(result)
+}
