75474669a8dcee3f5a3194c08dbb5f2278703678
JaehyunSong <tintstyle@gmail.com>
2019-01-29 16:07:23 +0900

    Add files via upload

--- R/GenerateURL.R
+++ R/GenerateURL.R
@@ -1,6 +1,7 @@
 GenerateURL <- function(file, Task = 3, Profile = 2,
                         Randomized = TRUE,
-                        DefaultURL = "http://www.jaysong.net/Qualtrics/SimpleConjoint/SimpleConjoint.php"){
+                        DefaultURL = "http://www.jaysong.net/Qualtrics/SimpleConjoint/SimpleConjoint.php",
+                        Design     = FALSE){
 
   df <- read.csv(file, header = FALSE, stringsAsFactors = FALSE)
   nA <- ncol(df)
@@ -8,29 +9,43 @@ GenerateURL <- function(file, Task = 3, Profile = 2,
   A  <- c()
   L  <- c()
 
-  j  <- 1
+  if (Design == FALSE) {
+    j  <- 1
 
-  for (i in 1:nA) {
-    Temp.vec <- df[, i]
-    A[i]  <- gsub("&", "%26", Temp.vec[1], fixed = TRUE)
-    A[i]  <- gsub("?", "%3F", Temp.vec[1], fixed = TRUE)
-    nL[i] <- sum(Temp.vec != "") - 1
-    L[j:(j + nL[i] - 1)] <- gsub("&", "%26", Temp.vec[2:(nL[i] + 1)], fixed = TRUE)
-    L[j:(j + nL[i] - 1)] <- gsub("?", "%3F", Temp.vec[2:(nL[i] + 1)], fixed = TRUE)
-    j <- j + nL[i]
+    for (i in 1:nA) {
+      Temp.vec <- df[, i]
+      A[i]  <- gsub("&", "%26", Temp.vec[1], fixed = TRUE)
+      A[i]  <- gsub("?", "%3F", Temp.vec[1], fixed = TRUE)
+      nL[i] <- sum(Temp.vec != "") - 1
+      L[j:(j + nL[i] - 1)] <- gsub("&", "%26",
+                                   Temp.vec[2:(nL[i] + 1)], fixed = TRUE)
+      L[j:(j + nL[i] - 1)] <- gsub("?", "%3F",
+                                   Temp.vec[2:(nL[i] + 1)], fixed = TRUE)
+      j <- j + nL[i]
+    }
+
+    nA.R <- paste0("nA=", nA)
+    nL.R <- paste0("nL[]=", paste(nL, collapse = "&nL[]="))
+    A.R  <- paste0("A[]=", paste(A, collapse = "&A[]="))
+    L.R  <- paste0("L[]=", paste(L, collapse = "&L[]="))
+    nPro <- paste0("nProfile=", Profile)
+    nTas <- paste0("nTask=", Task)
+    Rand <- paste0("AttrRand=", as.numeric(Randomized))
+
+    result <- paste0(DefaultURL, "?",
+                     paste(c(nTas, nPro, Rand, nA.R, nL.R, A.R, L.R),
+                           collapse = "&"))
+
+    cat(result)
   }
 
-  nA.R <- paste0("nA=", nA)
-  nL.R <- paste0("nL[]=", paste(nL, collapse = "&nL[]="))
-  A.R  <- paste0("A[]=", paste(A, collapse = "&A[]="))
-  L.R  <- paste0("L[]=", paste(L, collapse = "&L[]="))
-  nPro <- paste0("nProfile=", Profile)
-  nTas <- paste0("nTask=", Task)
-  Rand <- paste0("AttrRand=", as.numeric(Randomized))
+  if (Design == TRUE) {
+    DesignList <- list()
 
-  result <- paste0(DefaultURL, "?",
-                   paste(c(nTas, nPro, Rand, nA.R, nL.R, A.R, L.R),
-                         collapse = "&"))
+    for (i in 1:ncol(df)) {
+      DesignList[[df[1, i]]] <- df[-1, i][df[-1, i] != ""]
+    }
 
-  return(result)
+    return(DesignList)
+  }
 }
